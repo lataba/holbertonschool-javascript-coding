7a4ce37d51ff37c5bea3caab0b9eadd2d65e2246
lataba <ltballeste@gmail.com>
2023-10-13 19:45:51 -0700

    Task complete

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import controllerRouting from './routes/index';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const app = express();
 const port = 1245;
 
 controllerRouting(app);
 
-app.use('/', routes);
+app.listen(port, () => {
+});
 
 export default app;
